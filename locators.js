a41d1f4bd1d07d81308ba66c82a9d95966e490c1
Shahab Ali <96525045+syedshahabali93@users.noreply.github.com>
2022-06-21 10:45:31 +0500

    Add files via upload

---
+++ locators.js
@@ -0,0 +1,6 @@
+export const base_url = 'https://www.linkedin.com/';
+export const job_view_url = 'https://www.linkedin.com/jobs/view/';
+export const jobTitleLocator = '.jobs-unified-top-card__job-title';
+export const companySizeLocator = '.jobs-unified-top-card__job-insight';
+export const noMatchingJobsFound = '.jobs-search-two-pane__no-results-banner--expand';
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1. Install NodeJS
+2. Run command npm i playwright
+3. Run npm Install
+4. Run npm start to start execution
