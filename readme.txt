e60bd64e60af6a1bdc5a60bdb31882a774783f48
Shahab Ali <96525045+syedshahabali93@users.noreply.github.com>
2022-06-21 20:56:26 +0500

    Add files via upload

--- readme.txt
+++ readme.txt
@@ -2,3 +2,5 @@
 2. Run command npm i playwright
 3. Run npm Install
 4. Run npm start to start execution
+
+5. Run using increased memory for your scrapper using command npm startIncreasedMemory
--- test.js
+++ test.js
@@ -3,9 +3,11 @@ import fs from 'fs';
 import { base_url, job_view_url, jobTitleLocator, companySizeLocator, noMatchingJobsFound } from './locators.js';
 
 (async () => {
-    const pageLoadDelay = 5000;
+    const pageLoadDelay = 6000;
     const buttonPressDelay = 20000;
-    const jobsList = [];
+    let jobsList = [];
+    let jobPage;
+    let currentUrl = "";
     try {
         const browser = await playWright['chromium'].launch({
             headless: false
@@ -34,70 +36,83 @@ import { base_url, job_view_url, jobTitleLocator, companySizeLocator, noMatching
         let paginationValue = 0;
         let pageNo = 1;
         while (paginationExists) {
-            console.log("Page No. " + pageNo);
-            await delay(pageLoadDelay);
-            let jobIDsList = await homePage.evaluate(() => {
-                var temp = [];
-                var jobIDList = document.getElementsByClassName('jobs-search-results__list-item');
-
-                Array.prototype.forEach.call(jobIDList, function (el) {
-                    temp.push(el.getAttribute('data-occludable-job-id'));
-                });
-                return temp;
-            })
-
-            for (let index = 0; index < jobIDsList.length; index++) {
-                let jobObject = {};
-                let jobPage = await context.newPage();
-                let jobUrl = job_view_url + jobIDsList[index] + '/';
-                jobPage.goto(jobUrl);
-                await delay(pageLoadDelay);
-                await jobPage.waitForSelector(jobTitleLocator);
-                let jobTitleElement = await jobPage.$$(jobTitleLocator);
-                let jobTitle = await jobTitleElement[0].innerText();
-                await jobPage.waitForSelector(companySizeLocator);
-                let jobCompanySizeElement = await jobPage.$$(companySizeLocator);
-                let jobCompanySize = await jobCompanySizeElement[1].innerText();
-                jobObject['TITLE'] = jobTitle;
-                jobObject['COMPANY-SIZE'] = jobCompanySize;
-                jobObject['URL'] = jobUrl;
-                jobsList.push(jobObject);
-                await delay(pageLoadDelay);
-                await jobPage.close();
-            }
-
-            let currentUrl = homePage.url();
-            paginationValue = 25 + paginationValue;
-            currentUrl = currentUrl + '&start=' + paginationValue.toString();
-            homePage.goto(currentUrl);
-            await delay(pageLoadDelay);
-
-            console.log("Page Level Objects List: ");
-            console.log(jobsList);
-            console.log("\n");
-            pageNo = pageNo + 1;
-
-            let json = JSON.stringify(jobsList);
-
-            fs.appendFileSync('output.json', json, 'utf-8', function (err) {
-                if (err) {
-                    console.log('An error occurred while writing file.');
-                    return console.log(err);
-    
-                }
-                console.log('output.json has been saved.');
-            });
-    
             try {
-                let checkForLastPage = await homePage.$(noMatchingJobsFound);
-                if (checkForLastPage != null) {
-                    paginationExists = false;
+                console.log("Page No. " + pageNo);
+                await delay(pageLoadDelay);
+                let jobIDsList = await homePage.evaluate(() => {
+                    var temp = [];
+                    var jobIDList = document.getElementsByClassName('jobs-search-results__list-item');
+
+                    Array.prototype.forEach.call(jobIDList, function (el) {
+                        temp.push(el.getAttribute('data-occludable-job-id'));
+                    });
+                    return temp;
+                })
+
+                for (let index = 0; index < jobIDsList.length; index++) {
+                    let jobObject = {};
+                    jobPage = await context.newPage();
+                    let jobUrl = job_view_url + jobIDsList[index] + '/';
+                    jobPage.goto(jobUrl);
+                    await delay(pageLoadDelay);
+                    await jobPage.waitForSelector(jobTitleLocator);
+                    let jobTitleElement = await jobPage.$$(jobTitleLocator);
+                    let jobTitle = await jobTitleElement[0].innerText();
+                    await jobPage.waitForSelector(companySizeLocator);
+                    let jobCompanySizeElement = await jobPage.$$(companySizeLocator);
+                    let jobCompanySize = await jobCompanySizeElement[1].innerText();
+                    jobObject['TITLE'] = jobTitle;
+                    jobObject['COMPANY-SIZE'] = jobCompanySize;
+                    jobObject['URL'] = jobUrl;
+                    jobsList.push(jobObject);
+                    await delay(pageLoadDelay);
+                    await jobPage.close();
                 }
-            } catch (e) { }
+
+                currentUrl = homePage.url();
+                paginationValue = 25 + paginationValue;
+                currentUrl = currentUrl + '&start=' + paginationValue.toString();
+                homePage.goto(currentUrl);
+                await delay(pageLoadDelay);
+
+                console.log("Page Level Objects List: ");
+                console.log(jobsList);
+                console.log("\n");
+                pageNo = pageNo + 1;
+
+                let json = JSON.stringify(jobsList);
+
+                fs.appendFileSync('output.json', json, 'utf-8', function (err) {
+                    if (err) {
+                        console.log('An error occurred while writing file.');
+                        return console.log(err);
+
+                    }
+                    console.log('output.json has been saved.');
+                });
+
+                try {
+                    let checkForLastPage = await homePage.$(noMatchingJobsFound);
+                    if (checkForLastPage != null) {
+                        paginationExists = false;
+                    }
+                } catch (e) { }
+
+            } catch (e) {
+                jobsList = [];
+                const pages = context.pages();
+                if(pages.length > 1) {
+                    
+                    await jobPage.close();
+
+                }
+                homePage.goto(currentUrl);
+            }
         }
         console.log("Completed.");
         console.log("All Pages List: ");
         console.log(jobsList);
+        browser.close();
 
     } catch (exception) {
         console.log("Error Occurred");
